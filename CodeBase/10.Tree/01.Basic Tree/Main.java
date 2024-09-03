f32edefd94ff75083b1054f2e1b93a06f8563d9e
Karthikeyan K <karthikeyankdeveloper@gmail.com>
2024-09-03 12:21:36 +0530

    [UPDATE]-> Tree Data Structure {OneNote}

--- CodeBase/10.Tree/01.Basic Tree/Main.java
+++ CodeBase/10.Tree/01.Basic Tree/Main.java
@@ -1,12 +1,14 @@
 class Main{
     public static void main(String[] args) {
 
-        Tree tree = new Tree(10);
+        Tree tree = new Tree(1);
 
-        tree.insertLeft(tree.root, 12);
-        tree.insertRight(tree.root, 30);
-        tree.insertLeft(tree.root.left, 44);
-        tree.insertRight(tree.root.right, 40);
+        tree.insertLeft(tree.root, 2);
+        tree.insertRight(tree.root, 3);
+        tree.insertLeft(tree.root.left, 4);
+        tree.insertRight(tree.root.right, 6);
+        tree.insertRight(tree.root.left, 5);
+        tree.insertLeft(tree.root.right, 7);
         
     }
 }
