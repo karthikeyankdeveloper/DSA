9c60c84a814e6f9204135f4e706a6c6075828457
Karthikeyan K <karthikeyankdeveloper@gmail.com>
2024-09-04 21:59:33 +0530

    [feat]:Binary Search Tree {Codebase}

--- CodeBase/10.Tree/02.BinarySearchTree/BinarySearchTree.java
+++ CodeBase/10.Tree/02.BinarySearchTree/BinarySearchTree.java
@@ -1,3 +1,46 @@
 public class BinarySearchTree {
+
+    /**
+     * Inner Class
+     */
+    public class Node {
+
+        int data;
+        Node left,right;
+
+        public Node(int data){
+            this.data = data;
+        }
+    }
+
+    public Node root;
+
+    public BinarySearchTree(int key){
+        root = new Node(key);
+    }
+
+    public void inorder(Node root){
+        if(root==null) return;
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+
+    public Node insert(Node root,int data){
+
+        if(root==null) return new Node(data);
+
+        if(root.data>data){
+            root.left = insert(root.left, data);
+        }else{
+            root.right = insert(root.right, data);
+        }
+
+        return root;
+    }
+
+    public void search(int data){
+
+    }
     
 }
--- CodeBase/10.Tree/02.BinarySearchTree/Main.java
+++ CodeBase/10.Tree/02.BinarySearchTree/Main.java
@@ -1,3 +1,20 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        BinarySearchTree bst = new BinarySearchTree(50);
+        bst.insert(bst.root,20);
+        bst.insert(bst.root,25);
+        bst.insert(bst.root,15);
+        bst.insert(bst.root,7);
+        bst.insert(bst.root,67);
+        bst.insert(bst.root,65);
+        bst.insert(bst.root,88);
+        bst.insert(bst.root,99);
+        
+
+        bst.inorder(bst.root);
+        bst.search(0);
+    }
     
 }
