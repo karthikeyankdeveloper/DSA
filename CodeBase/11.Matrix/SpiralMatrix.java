d6e4af20ddef8e9270c86fd1b2bf7dd584fa7809
Karthikeyan K <karthikeyankdeveloper@gmail.com>
2024-11-19 20:57:36 +0530

    [Feat]: DSA Update

--- CodeBase/11.Matrix/SpiralMatrix.java
+++ CodeBase/11.Matrix/SpiralMatrix.java
@@ -1,3 +1,22 @@
 public class SpiralMatrix {
+
+    public static void main(String[] args) {
+
+        int[][] arr = {
+            {1,2,3,4,5},
+            {16,17,18,19,6},
+            {15,24,25,20,7},
+            {14,23,22,21,8},
+            {13,12,11,10,9}
+        };
+
+        travel(arr,0,0);   
+    }
+
+    private static void travel(int[][] arr,int start,int end){
+
+        
+
+    }
     
 }
